53b4c52808dff7424f26277dd7bcf7efc785258f
Leonardo Santana <hi@leonsantana.com>
2022-12-28 21:53:55 -0500

    added small animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls } from '@react-three/drei';
+import { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
 import './App.scss';
-import NameRound from './components/nameRound';
+// import NameRound from './components/nameRound';
 
 function App() {
+	const app = useRef();
+	const intro = useRef();
+	const name = useRef();
+	const col_1 = useRef();
+	const col_2 = useRef();
+
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			// intro
+			gsap.from(intro.current, { x: -20, opacity: 0 });
+			gsap.to(intro.current, {
+				x: 0,
+				opacity: 1,
+				// ease: 'power2.out',
+			});
+			gsap.to(intro.current, {
+				delay: 2,
+				x: 20,
+				opacity: 0,
+			});
+			// name
+			gsap.from(name.current, { x: -20, opacity: 0, delay: 2.5 });
+			// col_1
+			gsap.from(col_1.current, { x: -20, opacity: 0, delay: 2.7 });
+			// col_2
+			gsap.from(col_2.current, { x: -20, opacity: 0, delay: 2.9 });
+		}, app);
+
+		return () => ctx.revert();
+	}, []);
+
 	const data = [
 		{
 			project: 'Avenues Admisions',
@@ -43,7 +76,7 @@ function App() {
 		},
 	];
 	return (
-		<div className="App">
+		<div ref={app} className="App">
 			{/* <Canvas className="webgl">
 				<ambientLight intensity={0.5} /]>
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
@@ -54,8 +87,9 @@ function App() {
 			</Canvas> */}
 			<main>
 				<section>
-					<h1>Leonardo Santana</h1>
-					<div className="about-me">
+					<h1 ref={intro}>Welcome...</h1>
+					<h1 ref={name}>Leonardo Santana</h1>
+					<div ref={col_1} className="about-me">
 						<h2>About Me</h2>
 						<p>
 							I am a full stack software developer with a creative mindset. I
@@ -85,11 +119,11 @@ function App() {
 						</p>
 					</div>
 				</section>
-				<section>
+				<section ref={col_2}>
 					<h2>Selected Work</h2>
 					{data.map((item, index) => (
 						<div className="item" key={index.toString()}>
-							<a href={item.link} target="_blank">
+							<a href={item.link} target="_blank" rel="noreferrer">
 								<h3>{item.project}</h3>
 								<p className="client">{item.client}</p>
 								<p className="tech">{item.technology}</p>
